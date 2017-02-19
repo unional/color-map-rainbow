1b1b81b0fb7dea2b4b5c2c96d29520add50c11d9
Homa Wong <homawong@gmail.com>
2017-02-18 18:29:37 -0800

    Remove extra devDeps

--- scripts/setup-test-env.js
+++
@@ -1,6 +0,0 @@
-const LogManager = require('aurelia-logging')
-const ColorAppender = require('aurelia-logging-color').ColorAppender
-
-LogManager.addAppender(new ColorAppender())
-
-LogManager.setLevel(LogManager.logLevel.debug)
